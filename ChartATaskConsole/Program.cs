56805e52ecd21e1753fa570616b2635fa1a48b8d
Timothy Jones <t.jones@timmoth.com>
2019-10-19 11:32:38 +0100

    Created initial project structure

---
+++ ChartATaskConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChartATaskConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
