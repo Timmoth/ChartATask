2b4cae84a53e609988ff4627bb0bb808d4974e03
Timothy Jones <t.jones@timmoth.com>
2019-10-27 21:17:35 +0000

    Added AppOpen and AppClose

---
+++ Core/Models/Events/AppEvents/AppCloseEvent.cs
@@ -0,0 +1,27 @@
+﻿namespace ChartATask.Core.Models.Events
+{
+    public class AppCloseEvent : IEvent
+    {
+        public readonly string Name;
+
+        public AppCloseEvent(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $@"AppCloseEvent: {Name}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is AppCloseEvent appClosed && appClosed.Name == Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+    }
+}
---
+++ Core/Models/Events/AppEvents/AppOpenEvent.cs
@@ -0,0 +1,27 @@
+﻿namespace ChartATask.Core.Models.Events
+{
+    public class AppOpenEvent : IEvent
+    {
+        public readonly string Name;
+
+        public AppOpenEvent(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $@"AppOpenEvent: {Name}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is AppOpenEvent appOpened && appOpened.Name == Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+    }
+}
